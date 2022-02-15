e55ef9cb63ef1c55ab82c84953b2147b87c89705
Nikita.Gnezdilov <Nikita.Gnezdilov@waveaccess.global>
2022-02-15 16:27:54 +0500

    +

--- src/bot/event-handler.ts
+++ src/bot/event-handler.ts
@@ -1,11 +1,8 @@
 class DurkaEventHandler {
     async messageNew(bot:VkBot) {
-        let res = await bot.event('message_new'); // Ловим последнее событие message_new
-        return res;
-    }
-    async userId(bot:VkBot) {
-        let msg = await this.messageNew(bot); // Получить последнее сообщение
-        
+        bot.event('message_new', res => {
+            
+        });
     }
 }
 
